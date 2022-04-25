db5b804343be598476ff60157ababb0df8c06059
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2022-04-25 11:01:57 -0600

    [main] - tag list now works much better and fits with the design. It is still missing pagination - TT

--- Sources/aPrincipalEngineer/SparrowTheme.swift
+++ Sources/aPrincipalEngineer/SparrowTheme.swift
@@ -7,6 +7,7 @@
 
 import Publish
 import Plot
+import Foundation
 
 extension Theme where Site == APrincipalEngineer {
     static var sparrow: Self {
@@ -38,42 +39,89 @@ extension Theme where Site == APrincipalEngineer {
                              context: PublishingContext<Site>) throws -> HTML? {
             SitePage(sitePage: page,
                      context: context) {
-                H1("Browse all tags")
-                List(page.tags.sorted()) { tag in
-                    ListItem {
-                        Link(tag.string,
-                             url: context.site.url.appendingPathComponent(context.site.path(for: tag).absoluteString).appendingPathComponent("index.html").absoluteString
-                        )
-                    }
-                    .class("tag")
-                }
-                .class("all-tags")
+                Div {
+                    Div {
+                        H1("Browse all tags")
+                        List(page.tags.sorted()) { tag in
+                            ListItem {
+                                Link(tag.string,
+                                     url: context.site.url.appendingPathComponent(context.site.path(for: tag).absoluteString).appendingPathComponent("index.html").absoluteString
+                                )
+                            }
+                            .class("tag")
+                        }
+                        .class("all-tags")
+                    }.id("page-content")
+                        .class("row")
+                }.class("content-outer")
             }.html
         }
 
         func makeTagDetailsHTML(for page: TagDetailsPage,
                                 context: PublishingContext<Site>) throws -> HTML? {
-            SitePage(sitePage: page,
-                     context: context) {
-                H1 {
-                    Text("Tagged with ")
-                    Span(page.tag.string).class("tag")
-                }
+            let visibleDateFormatter: DateFormatter = {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "MMM dd, yyyy"
+                return formatter
+            }()
 
-                Link("Browse all tags",
-                     url: context.site.url.appendingPathComponent(context.site.tagListPath.absoluteString).appendingPathComponent("index.html").absoluteString
-                )
-                    .class("browse-all")
-
-                //                        ItemList(
-                //                            items: context.items(
-                //                                taggedWith: page.tag,
-                //                                sortedBy: \.date,
-                //                                order: .descending
-                //                            ),
-                //                            site: context.site
-                //                        )
-                //                    }
+            let timeElementDateFormatter: DateFormatter = {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-dd-MM"
+                return formatter
+            }()
+            return SitePage(sitePage: page,
+                            context: context) {
+                Div {
+                    Div {
+                        Div {
+                            let components: [Component] = context
+                                                            .items(taggedWith: page.tag, sortedBy: \.date, order: .descending)
+                                                            .map { post in
+                                Article {
+                                    Div {
+                                        Link(url: context.site.url.appendingPathComponent(post.path.absoluteString).appendingPathComponent("index.html").absoluteString) {
+                                            H1(post.title)
+                                        }
+                                        Paragraph {
+                                            Element(name: "time") {
+                                                Text(visibleDateFormatter.string(from: post.date))
+                                            }.attribute(named: "datetime", value: timeElementDateFormatter.string(from: post.date))
+                                                .class("date")
+                                            if !post.tags.isEmpty {
+                                                Text(" / ")
+                                                Span {
+                                                    let breadcrumbs: [Component] = post.tags.flatMap { [Link($0.string, url: context.site.url.appendingPathComponent("tags").appendingPathComponent($0.string).appendingPathComponent("index.html").absoluteString), Text(" / ")] as [Component] }.dropLast()
+                                                    ComponentGroup(members: breadcrumbs)
+                                                }.class("categories")
+                                            }
+                                        }.class("post-meta")
+                                    }.class("entry-header cf")
+                                    if let imagePath = post.imagePath {
+                                        Div {
+                                            Image(url: context.site.url.appendingPathComponent("images").appendingPathComponent(imagePath.absoluteString), description: "post-image")
+                                        }.class("post-thumb")
+                                    }
+                                    Div {
+                                        Paragraph(post.description)
+                                    }.class("post-content")
+                                }.class("post")
+                            }
+                            ComponentGroup(members: components)
+                        }.id("primary")
+                            .class("eight columns")
+                        Div {
+                            Element(name: "aside") {
+                                Link("Browse all tags",
+                                     url: context.site.url.appendingPathComponent(context.site.tagListPath.absoluteString).appendingPathComponent("index.html").absoluteString
+                                )
+                                    .class("browse-all")
+                            }.id("sidebar")
+                        }.id("secondary")
+                            .class("four columns end")
+                    }.id("page-content")
+                        .class("row")
+                }.class("content-outer")
             }.html
         }
     }
