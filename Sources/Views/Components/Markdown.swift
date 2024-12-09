5345ce73ac578ef06b398aaafa449b7b7899c9ef
Tyler Thompson <tyler.keith.thompson@gmail.com>
2024-12-08 19:50:15 -0700

    [vapor] - Allow for user specified languages - TT

--- Sources/Views/Components/Markdown.swift
+++ Sources/Views/Components/Markdown.swift
@@ -9,10 +9,30 @@ import Elementary
 import Ink
 
 struct Markdown: HTML {
+    var parser: MarkdownParser {
+        var parser = MarkdownParser()
+
+        let modifier = Modifier(target: .codeBlocks) { html, markdown in
+            let language: String = {
+                // Check if it's a fenced code block
+                if markdown.hasPrefix("```") {
+                    let firstLine = markdown.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true).first ?? ""
+                    let identifier = firstLine.drop(while: { $0 == "`" }).trimmingCharacters(in: .whitespaces)
+                    return identifier.isEmpty ? "swift" : identifier
+                }
+                // Default to "swift" for non-fenced blocks
+                return "swift"
+            }()
+            return "<div class=\"language-\(language)\">" + html + "</div>"
+        }
+
+        parser.addModifier(modifier)
+        return parser
+    }
     let markdown: String
     var content: some HTML {
-        div(.class("language-swift")) {
-            HTMLRaw(MarkdownParser().html(from: markdown))
+        div(.class("language-swift")) { // will be overriden if a different language is detected, but helpful for inline code snippets
+            HTMLRaw(parser.html(from: markdown))
         }
     }
 }
